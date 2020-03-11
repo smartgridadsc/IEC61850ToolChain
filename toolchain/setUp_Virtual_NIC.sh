d0a21fefe438d94fef87f63d5914ad41e27f8476
liyuan3520 <liyuan0125@gmail.com>
2020-03-10 20:59:05 -0700

    complete 20 ieds experiment

--- toolchain/setUp_Virtual_NIC.sh
+++ toolchain/setUp_Virtual_NIC.sh
@@ -1,2 +1,43 @@
-sudo ip link add link ens33 address "00:11:22:33:44:55" ens33.1 type macvlan mode bridge
-sudo ifconfig ens33.1 up
+#sudo ip link add link ens33 address "00:11:22:33:44:01" ens33.1 type macvlan mode bridge
+#sudo ifconfig ens33.1 up
+#sudo ip link add link ens33 address "00:11:22:33:44:02" ens33.2 type macvlan mode bridge
+#sudo ifconfig ens33.2 up
+#sudo ip link add link ens33 address "00:11:22:33:44:03" ens33.3 type macvlan mode bridge
+#sudo ifconfig ens33.3 up
+#sudo ip link add link ens33 address "00:11:22:33:44:04" ens33.4 type macvlan mode bridge
+#sudo ifconfig ens33.4 up
+#sudo ip link add link ens33 address "00:11:22:33:44:05" ens33.5 type macvlan mode bridge
+#sudo ifconfig ens33.5 up
+#sudo ip link add link ens33 address "00:11:22:33:44:06" ens33.6 type macvlan mode bridge
+#sudo ifconfig ens33.6 up
+#sudo ip link add link ens33 address "00:11:22:33:44:07" ens33.7 type macvlan mode bridge
+#sudo ifconfig ens33.7 up
+#sudo ip link add link ens33 address "00:11:22:33:44:08" ens33.8 type macvlan mode bridge
+#sudo ifconfig ens33.8 up
+#sudo ip link add link ens33 address "00:11:22:33:44:09" ens33.9 type macvlan mode bridge
+#sudo ifconfig ens33.9 up
+#sudo ip link add link ens33 address "00:11:22:33:44:10" ens33.10 type macvlan mode bridge
+#sudo ifconfig ens33.10 up
+
+sudo ip link add link ens33 address "00:11:22:33:44:11" ens33.11 type macvlan mode bridge
+sudo ifconfig ens33.11 up
+sudo ip link add link ens33 address "00:11:22:33:44:12" ens33.12 type macvlan mode bridge
+sudo ifconfig ens33.12 up
+sudo ip link add link ens33 address "00:11:22:33:44:13" ens33.13 type macvlan mode bridge
+sudo ifconfig ens33.13 up
+sudo ip link add link ens33 address "00:11:22:33:44:14" ens33.14 type macvlan mode bridge
+sudo ifconfig ens33.14 up
+sudo ip link add link ens33 address "00:11:22:33:44:15" ens33.15 type macvlan mode bridge
+sudo ifconfig ens33.15 up
+sudo ip link add link ens33 address "00:11:22:33:44:16" ens33.16 type macvlan mode bridge
+sudo ifconfig ens33.16 up
+sudo ip link add link ens33 address "00:11:22:33:44:17" ens33.17 type macvlan mode bridge
+sudo ifconfig ens33.17 up
+sudo ip link add link ens33 address "00:11:22:33:44:18" ens33.18 type macvlan mode bridge
+sudo ifconfig ens33.18 up
+sudo ip link add link ens33 address "00:11:22:33:44:19" ens33.19 type macvlan mode bridge
+sudo ifconfig ens33.19 up
+sudo ip link add link ens33 address "00:11:22:33:44:20" ens33.20 type macvlan mode bridge
+sudo ifconfig ens33.20 up
+
+
